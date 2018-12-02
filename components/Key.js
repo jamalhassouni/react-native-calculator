2c18cdd5c30b53e932e0cb7b42bd7f91dfd8b9c2
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-02 15:34:48 +0100

    Using vector icons to support display icons

--- components/Key.js
+++ components/Key.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React from "react";
+import { Text, View, TouchableHighlight, StyleSheet } from "react-native";
 import {
-  Text,
-  View,
-  TouchableHighlight,
-  StyleSheet,
-} from 'react-native';
+  Feather,
+  MaterialIcons,
+  MaterialCommunityIcons
+} from "@expo/vector-icons";
 
 export default class Key extends React.Component {
   constructor(props) {
     super(props);
     this._onPress = this._onPress.bind(this);
-
   }
   _onPress() {
     this.props.echoSymbol(this.props.symbol);
   }
-
+  renderIconOrStirng = () => {
+    if (this.props.setIcon) {
+      switch (this.props.iconType) {
+        case "Feather":
+          return <Feather name={this.props.name} size={22} color="white" />;
+        case "MaterialIcons":
+          return (
+            <MaterialIcons name={this.props.name} size={22} color="white" />
+          );
+        case "MaterialCommunityIcons":
+          return (
+            <MaterialCommunityIcons
+              name={this.props.name}
+              size={22}
+              color="white"
+            />
+          );
+      }
+    } else {
+      return this.props.symbol;
+    }
+  };
   render() {
-    const back= this.props.backgroundColor || '#4f9a94';
-
+    const back = this.props.backgroundColor || "#4f9a94";
     return (
       <TouchableHighlight
-        style={this.props.op ? [styles.key,{backgroundColor:back}] : styles.key}
-        onPress={this._onPress}>
+        style={
+          this.props.op ? [styles.key, { backgroundColor: back }] : styles.key
+        }
+        onPress={this._onPress}
+      >
         <View>
           <Text style={[styles.keytext, this.props.op && styles.opkeytext]}>
-            {this.props.symbol}
+            {this.renderIconOrStirng()}
           </Text>
         </View>
       </TouchableHighlight>
@@ -40,16 +62,16 @@ const styles = StyleSheet.create({
     alignSelf: "stretch",
     justifyContent: "center",
     width: 60,
-    height: 60,
+    height: 60
   },
   keytext: {
     fontSize: 20,
-    color: '#ffffff',
+    color: "#ffffff"
   },
   opkey: {
-    marginRight: 10,
+    marginRight: 10
   },
   opkeytext: {
-    color: '#ffffff',
-  },
+    color: "#ffffff"
+  }
 });
